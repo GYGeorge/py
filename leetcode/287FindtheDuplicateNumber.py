fce6b69fd308e839911aaef9ffef3bc408d25fab
GYGeorge <elxuckit@gmail.com>
2020-06-28 20:14:41 +0800

    add FindDuplicateNumber

---
+++ leetcode/287FindtheDuplicateNumber.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+        Given an array nums containing n + 1 integers 
+        where each integer is between 1 and n (inclusive), 
+        prove that at least one duplicate number must exist. 
+        Assume that there is only one duplicate number, find the duplicate one.
+    """
+    def FindtheDuplicateNumber(self, nums):
+        """
+            环检测算法Floyd's the Tortoise and Hare
+        """
+        hare = tortoise = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[tortoise]]
+            if hare == tortoise:
+                break
+        hare = nums[0]
+        while hare != tortoise:
+            hare = nums[hare]
+            tortoise = nums[tortoise]
+        return hare
+    
+if __name__ == "__main__":
+    array = [1,3,4,2,2]
+    so = Solution()
+    print(so.FindtheDuplicateNumber(array))
